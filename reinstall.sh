17ef04be458fa8e21f347b5d4c8578bc061cacfb
ywzgt <92680591+ywzgt@users.noreply.github.com>
2025-05-10 18:17:26 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 PATH="/c/Users/$(id -un)/AppData/Local/Microsoft/WindowsApps:$PATH"
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\InstallCleanup.exe"
+#"C:\Program Files (x86)\Microsoft Visual Studio\Installer\InstallCleanup.exe"
 
 for sdk in `yes | winget list | grep -i WindowsSDK | cut -d' ' -f7`
 do
