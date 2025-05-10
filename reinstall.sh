f2dc755c06ec2c5efe6dbbaa4a05fafdc8725399
ywzgt <92680591+ywzgt@users.noreply.github.com>
2025-05-10 20:38:32 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
-PATH="/c/Users/$(id -un)/AppData/Local/Microsoft/WindowsApps:$PATH"
+PATH="$PATH:/c/Users/$(id -un)/AppData/Local/Microsoft/WindowsApps"
 #"C:\Program Files (x86)\Microsoft Visual Studio\Installer\InstallCleanup.exe"
 
-for sdk in `yes | winget list | grep -i WindowsSDK | cut -d' ' -f7`
-do
-	[[ $sdk ]] || continue
-	winget uninstall $sdk
-done
-rm -rf "C:\Program Files (x86)\Windows Kits\10"
+#for sdk in `yes | winget list | grep -i WindowsSDK | cut -d' ' -f7`
+#do
+#	[[ $sdk ]] || continue
+#	winget uninstall $sdk
+#done
+#rm -rf "C:\Program Files (x86)\Windows Kits\10"
 
 yes | winget install --id Microsoft.VisualStudio.2022.Community --verbose --override "--passive --config $PWD/vsconfig.json"
